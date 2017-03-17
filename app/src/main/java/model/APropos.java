4af1a993afb744246d04ed197d202d982549440e
Jean-Baptiste Soyez <enknower@gmail.com>
2017-03-17 10:22:13 +0100

    création de l'activité correspondante à la vue a_propos

---
+++ app/src/main/java/model/APropos.java
@@ -0,0 +1,15 @@
+package model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.xylphid.betclic.R;
+
+public class APropos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_propos);
+    }
+}
