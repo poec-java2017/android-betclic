915e4dff3688ded3107b2edbdffc0570076567b9
Anthony Periquet <anthony@periquet.net>
2017-03-17 09:53:42 +0100

    Add actionbar to event activity

--- app/src/main/java/net/xylphid/betclic/EventActivity.java
+++ app/src/main/java/net/xylphid/betclic/EventActivity.java
@@ -1,19 +1,26 @@
 package net.xylphid.betclic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventActivity extends AppCompatActivity {
+public class EventActivity extends RootActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.tbHeader);
+        if (null != toolbar) {
+            setSupportActionBar(toolbar);
+        }
 
         List<String> betList = new ArrayList<>();
         betList.add("BetChoice - QuoteChoice / Pseudo : SumPlayed");
@@ -25,5 +32,24 @@ public class EventActivity extends AppCompatActivity {
         RecyclerView rvBetList = (RecyclerView) findViewById(R.id.rvBetList);
         rvBetList.setLayoutManager(new LinearLayoutManager(this));
         rvBetList.setAdapter(new BetAdapter(betList));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.header, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuAbout:
+                //action
+                return true;
+            case R.id.menuSignout:
+                //action
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
